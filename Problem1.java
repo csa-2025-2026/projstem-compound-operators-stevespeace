3316bd3253cf6b599bf9c3c6e0695b078c6ae78c
Steven Nguyen <149844435+stevespeace@users.noreply.github.com>
2025-09-11 17:38:59 +0000

    hi

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,41 @@
+public class Problem1
+{
+  public static void main(String[] args)
+  {
+  
+  
+     int n = 24;
+
+        System.out.println("Inital value: 24" + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+n++;
+        System.out.println("number is now " + n);
+
+
+
+
+
+  }
+}
 
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,24 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    
 
+    int n = 678;
+
+    System.out.println("Three digit number: " + n);
+
+int a = (n % 10);
+
+    System.out.println("Here are the digits: ");
+    System.out.println(a);
+    int b = (a /100);
+    System.out.println(b);
+ int c = (b /1000);
+System.out.println(c);
+
+
+
+
+  }
+}
